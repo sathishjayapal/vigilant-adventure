616de04f4a9181f94b4c3e061f3afc522f93df22
Sathish Jayapal <sjayapal@skminfotech.com>
2019-02-10 04:47:14 -0600

    Pre blog publish

--- bootstaticcontentsamplerweb/src/main/java/org/bootstaticsampler/config/BootSamplerWebAppWebConfigurer.java
+++ bootstaticcontentsamplerweb/src/main/java/org/bootstaticsampler/config/BootSamplerWebAppWebConfigurer.java
@@ -9,14 +9,8 @@ public class BootSamplerWebAppWebConfigurer implements WebMvcConfigurer{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pages/", "/error/","/images");
     }
-//        registry.addResourceHandler("/pages/","/error/")
-//                .addResourceLocations("classpath:/static/", "classpath:/pages/",
-//                        "classpath:/error/", "classpath:/static/images/")
-//                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS)
-//                        .cachePublic()).resourceChain(true);
-//    }
-//
-    @Override
+
+  @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
                 .setViewName("/main/index.html");
